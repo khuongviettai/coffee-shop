347a3949e0f493bced49537d2983e87078747ed6
khuongviettai <khuongviettai@outlook.com>
2023-02-23 14:03:14 +0700

    update

--- client/src/pages/SingleProduct.jsx
+++ client/src/pages/SingleProduct.jsx
@@ -14,6 +14,9 @@ const SingleProduct = () => {
   const [product, setProduct] = useState({});
   const [loading, setLoading] = useState(true);
   useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  useEffect(() => {
     axiosLink
       .get(`/api/products/${id}`)
       .then(({ data }) => {
